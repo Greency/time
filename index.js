9b940a0ef2dea3d730339c6b7bb81f3143582daa
chenyong <1015561170@qq.com>
2018-11-14 22:58:49 +0800

    feat: add function betweenDays

--- index.js
+++ index.js
@@ -19,11 +19,11 @@ const time = (function () {
             this._date = parseDate(date);
             this.init();
         }
-        
+
         /**
          * 初始化日期的数据
          *  */
-        init(){
+        init() {
             this._Y = this._date.getFullYear();
             this._M = this._date.getMonth() + 1;
             this._D = this._date.getDate();
@@ -36,38 +36,38 @@ const time = (function () {
          * 格式化日期
          * @param {String} str 按照str的格式来格式化日期 
          */
-        format(str){
-            if(!typeof str === 'string') new Error(` '${str}' 不是一个字符串！`);
+        format(str) {
+            if (!typeof str === 'string') new Error(` '${str}' 不是一个字符串！`);
 
-            return str.replace(REG_FORMAT, (match)=>{
-                switch(match){
+            return str.replace(REG_FORMAT, (match) => {
+                switch (match) {
                     case 'YYYY':
                         return this._Y;
-                    case 'YY': 
+                    case 'YY':
                         return String(this._Y).slice(-2);
                     case 'MM':
                         return this._M < 10 ? `0${this._M}` : this._M;
                     case 'M':
                         return this._M;
-                    case 'DD': 
+                    case 'DD':
                         return this._D < 10 ? `0${this._D}` : this._D;
                     case 'D':
                         return this._D;
-                    case 'hh': 
+                    case 'hh':
                         return this._h < 10 ? `0${this._h}` : this._h;
                     case 'h':
                         return this._h;
                     case 'mm':
                         return this._m < 10 ? `0${this._m}` : this._m;
-                    case 'm': 
+                    case 'm':
                         return this._m;
                     case 'ss':
                         return this._s < 10 ? `0${this._s}` : this._s;
                     case 's':
                         return this._s;
-                    default: 
+                    default:
                         new Error(` '${match}' 解析出错！`);
-                }   
+                }
             });
         }
 
@@ -76,8 +76,17 @@ const time = (function () {
          * @param {*} date 
          * @return {Boolean} 
          */
-        isBefore(date){
+        isBefore(date) {
             return this._date.getTime() - parseDate(date).getTime() < 0 ? true : false;
+        }
+
+        /**
+         * 计算两个日期之间的天数
+         * @param {*} date 
+         * @return {Number}
+         */
+        betweenDays(date) {
+            return parseInt((parseDate(date).getTime() - this._date.getTime()) / (1000 * 60 * 60 * 24));
         }
     }
 
