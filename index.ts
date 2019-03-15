3bc010e8fae0dd8d87d7ec1a26f8890fc353b521
chenyong <1015561170@qq.com>
2019-03-15 11:24:46 +0800

    feat: 替换计算时间差的旧接口

---
+++ index.ts
@@ -0,0 +1,14 @@
+export const time = (()=>{
+
+
+    class Time{
+        constructor(date: String | Date | any){
+            
+        }
+
+    }   
+
+    return function(date){
+        return new Time(date);
+    }
+})();
